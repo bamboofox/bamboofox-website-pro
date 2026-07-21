4214a66f3d03d015ec0196fb69fa5d8b31a88e5d
Elvis Dragon Mao <info@elvismao.com>
2026-07-21 22:51:58 +0800

    site: redesign the BambooFox public interface

---
+++ src/consts.js
@@ -0,0 +1,17 @@
+export const SITE_NAME = "BambooFox";
+export const CLUB_NAME = "NYCU Cyber Security Club";
+export const CLUB_NAME_ZH = "交大網路安全策進會";
+export const SITE_TITLE = `${SITE_NAME}｜${CLUB_NAME} ${CLUB_NAME_ZH}`;
+export const SITE_DESCRIPTION = "交大網路安全策進會（NYCU Cyber Security Club）BambooFox 成立於 2014 年，定期舉辦資安課程、參與 CTF 競賽並推動跨校技術交流。";
+export const DEFAULT_SOCIAL_IMAGE = "/brand/logo-square.webp";
+
+export const CALENDAR_FEED_URL = "https://calendar.google.com/calendar/ical/nctucscbamboofox%40gmail.com/public/basic.ics";
+
+export const CALENDAR_URL = "https://calendar.google.com/calendar/embed?src=nctucscbamboofox%40gmail.com&ctz=Asia%2FTaipei";
+
+export const SOCIAL_LINKS = {
+	facebook: "https://www.facebook.com/NCTUCSC",
+	instagram: "https://www.instagram.com/nycu.bamboofox/",
+	discord: "https://discord.com/invite/UCFapuc4jj",
+	email: "mailto:nctucscbamboofox@gmail.com"
+};
